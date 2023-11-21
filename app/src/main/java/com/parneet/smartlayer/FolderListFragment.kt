7b42f2942136e225ae255377982eb8f863fdab80
parneet-guraya <gurayaparneet@gmail.com>
2023-11-21 20:13:11 +0530

    use card view with linear list for folder view
    Signed-off-by: parneet-guraya <gurayaparneet@gmail.com>

--- app/src/main/java/com/parneet/smartlayer/FolderListFragment.kt
+++ app/src/main/java/com/parneet/smartlayer/FolderListFragment.kt
@@ -14,6 +14,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.parneet.smartlayer.databinding.FragmentFolderListBinding
 import kotlinx.coroutines.launch
 
@@ -65,7 +66,7 @@ class FolderListFragment : Fragment() {
                 goToVideoList(bucketId)
             }
 
-            binding.foldersRecyclerView.layoutManager = GridLayoutManager(requireContext(), 3)
+            binding.foldersRecyclerView.layoutManager = LinearLayoutManager(requireContext()) // GridLayoutManager(requireContext(), 3)
             binding.foldersRecyclerView.adapter = adapter
         }
     }
