802ccbb8e9f8d773ce90845b9947fef9023fb18a
parneet-guraya <gurayaparneet@gmail.com>
2023-11-23 03:33:56 +0530

    improve permission handling logic
    Signed-off-by: parneet-guraya <gurayaparneet@gmail.com>

--- app/src/main/java/com/parneet/smartlayer/FolderListFragment.kt
+++ app/src/main/java/com/parneet/smartlayer/FolderListFragment.kt
@@ -3,6 +3,7 @@ package com.parneet.smartlayer
 import android.Manifest
 import android.app.AlertDialog
 import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +11,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
@@ -21,6 +23,7 @@ import kotlinx.coroutines.launch
 class FolderListFragment : Fragment() {
     private var _binding: FragmentFolderListBinding? = null
     private val binding get() = _binding!!
+    private val requestPermissionLauncher = requestPermissionLauncher()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,39 +35,33 @@ class FolderListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        println("onViewCreated called")
         requestPermissionIfNeeded()
     }
 
     private fun requestPermissionIfNeeded() {
+        println("requestPermissionIfNeeded called")
         if (ContextCompat.checkSelfPermission(
                 requireContext(),
-                Manifest.permission.READ_MEDIA_VIDEO
+                getReadMediaPermission()
             ) == PackageManager.PERMISSION_GRANTED
         ) {
             loadVideoFolders()
         } else {
-            requestPermissionLauncher().launch(Manifest.permission.READ_MEDIA_VIDEO)
+            requestPermissionLauncher.launch(getReadMediaPermission())
         }
     }
 
     private fun requestPermissionLauncher(): ActivityResultLauncher<String> {
         return registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            println("launch result callback")
             if (isGranted) {
                 // show folders
                 loadVideoFolders()
             } else {
-                AlertDialog.Builder(requireContext())
-                    .setCancelable(false)
-                    .setTitle("Need Permission!")
-                    .setMessage("Need permission to read media from the phone to display content here.")
-                    .setPositiveButton("Ok") { _, _ ->
-                        requestPermissionLauncher().launch(Manifest.permission.READ_MEDIA_VIDEO)
-                    }.setNegativeButton("Cancel") { _, _ ->
-                        requireActivity().finish()
-                    }
-                    .show()
                 Toast.makeText(requireContext(), "Permission Denied", Toast.LENGTH_SHORT)
                     .show()
+                findNavController().navigate(R.id.permissionDeniedFragment)
             }
         }
     }
@@ -85,6 +82,14 @@ class FolderListFragment : Fragment() {
     private fun goToVideoList(bucketId: String) {
         val bundle = Bundle().also { bundle -> bundle.putString(EXTRA_BUCKET_ID, bucketId) }
         findNavController().navigate(R.id.action_folderListFragment_to_videoFolderFragment, bundle)
+    }
+
+    private fun getReadMediaPermission(): String {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            Manifest.permission.READ_MEDIA_VIDEO
+        } else {
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        }
     }
 
     override fun onDestroyView() {
---
+++ app/src/main/java/com/parneet/smartlayer/PermissionDeniedFragment.kt
@@ -0,0 +1,69 @@
+package com.parneet.smartlayer
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.provider.Settings
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.parneet.smartlayer.databinding.FragmentPermissionDeniedBinding
+
+class PermissionDeniedFragment : Fragment() {
+    private var _binding: FragmentPermissionDeniedBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentPermissionDeniedBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.openAppSettings.setOnClickListener { openSettings() }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        checkIfGranted()
+    }
+
+    private fun checkIfGranted() {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                getReadMediaPermission()
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            findNavController().navigate(R.id.action_permissionDeniedFragment_to_folderListFragment)
+        }
+    }
+
+    private fun getReadMediaPermission(): String {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            Manifest.permission.READ_MEDIA_VIDEO
+        } else {
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        }
+    }
+
+    private fun openSettings() {
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+        val uri = Uri.fromParts("package", requireActivity().packageName, null)
+        intent.data = uri
+        startActivity(intent)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
