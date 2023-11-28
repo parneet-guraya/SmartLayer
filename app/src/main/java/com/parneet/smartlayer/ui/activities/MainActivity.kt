a4ae9162f8f2a4402a9d9c26615151066d83d784
parneet-guraya <gurayaparneet@gmail.com>
2023-11-28 06:32:10 +0530

    add overflow menu in the app bar
    Signed-off-by: parneet-guraya <gurayaparneet@gmail.com>

--- app/src/main/java/com/parneet/smartlayer/ui/activities/MainActivity.kt
+++ app/src/main/java/com/parneet/smartlayer/ui/activities/MainActivity.kt
@@ -2,6 +2,8 @@ package com.parneet.smartlayer.ui.activities
 
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -20,11 +22,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setSupportActionBar(binding.topAppBar)
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
         binding.topAppBar.setupWithNavController(navController)
+        setSupportActionBar(binding.topAppBar)
+
         navController.addOnDestinationChangedListener { controller, destination, arguments ->
             binding.topAppBar.title = when (destination.id) {
                 R.id.folderListFragment -> {
@@ -33,6 +36,22 @@ class MainActivity : AppCompatActivity() {
 
                 else -> getString(R.string.app_name)
             }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.action_bar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.about -> {
+                println("about")
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
