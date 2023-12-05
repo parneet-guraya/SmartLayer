aa3bd724842bf82ab2934f2510fe883fa74877e7
parneet-guraya <gurayaparneet@gmail.com>
2023-12-05 05:37:23 +0530

    add parcelable generator
    Signed-off-by: parneet-guraya <gurayaparneet@gmail.com>

--- app/src/main/java/com/parneet/smartlayer/model/Video.kt
+++ app/src/main/java/com/parneet/smartlayer/model/Video.kt
@@ -1,11 +1,15 @@
 package com.parneet.smartlayer.model
 
 import android.net.Uri
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class Video(
     val id: Long,
     val title: String,
     val duration: Int,
     val uri: Uri
-)
+) : Parcelable
+
 data class Subtitle(val uri: Uri?, val title: String?)
